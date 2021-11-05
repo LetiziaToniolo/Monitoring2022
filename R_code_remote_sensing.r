f85e9786d5c2685a741db43c19bfea15c2b506f7
LetiziaToniolo <92572869+LetiziaToniolo@users.noreply.github.com>
2021-11-05 11:05:06 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -6,3 +6,30 @@
 install.packages("raster")
 
 library(raster)
+
+setwd("C:/lab/") 
+l2011 <- brick("p224r63_2011.grd")
+
+# let's plot the green band
+plot(l2011$B2_sre)
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011$B2_sre, col=cl)
+
+#change the coloraRampPalette with dark green, green and light green
+cl <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(l2011$B2_sre, col=cl)
+
+# do the same for the blue band
+cl <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=cl)
+
+# plot both images in in just one multiframe graph
+par(mfraw=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# now 2 raws and 1 column
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
